42b0b724530eb7d0553e8a4036ec9fc04a2f490e
dev-techguy <dev.techguy@gmail.com>
2021-06-08 16:24:57 +0300

    :ok_hand: Updating code due to code review changes.

--- database/migrations/1623146488024_posts.ts
+++ database/migrations/1623146488024_posts.ts
@@ -3,18 +3,28 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class Posts extends BaseSchema {
   protected tableName = 'posts'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.integer("user_id", 180).notNullable()
-      table.integer("forum_id").nullable()
+      table.increments('id').primary()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('users.id')
+        .notNullable()
+        .onDelete('CASCADE') // delete posts when user is deleted
+      table
+        .integer('forum_id')
+        .unsigned()
+        .references('forums.id')
+        .notNullable()
+        .onDelete('CASCADE') // delete posts when forum is deleted
       table.string("title", 255).notNullable()
       table.string("content", 255).notNullable()
       table.timestamps(true)
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
--- database/migrations/1623146816505_forums.ts
+++ database/migrations/1623146816505_forums.ts
@@ -3,17 +3,22 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class Forums extends BaseSchema {
   protected tableName = 'forums'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.integer("user_id", 180).notNullable()
+      table.increments('id').primary()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('users.id')
+        .notNullable()
+        .onDelete('CASCADE') // delete forums when user is deleted
       table.string("title", 255).notNullable()
       table.string("description", 255).notNullable()
       table.timestamps(true)
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
