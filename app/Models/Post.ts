2f7861950921cd4ca1b722aabee07a667de5dc07
dev-techguy <dev.techguy@gmail.com>
2021-06-08 13:16:11 +0300

    Setting database migrations

---
+++ app/Models/Post.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public user_id: number
+
+  @column()
+  public forum_id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public content: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
